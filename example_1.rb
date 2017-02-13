135208da748baf2bf6f567a4fa5c68c0d2452e83
Alex Zhukov <baron.pampa@gmail.com>
2017-02-14 00:30:09 +0300

    rewrite midi input in async mode to support multiple input devices

--- example_1.rb
+++ example_1.rb
@@ -1,8 +1,29 @@
-require_relative 'lib/synth'
-require_relative 'setup'
+require_relative 'boot'
 
-# print note on/off messages
-plug :circuit do |n| 
-	n.pp if n.note?
+i = ALSA::Input.new
+
+i.open("hw:3,0,0")
+i.open("hw:4,0,0")
+
+i.listen do |port, n|
+    puts "#{port}, #{n.inspect}" unless n == 0xf8.chr
 end
 
+#o = ALSA::Output.open("hw:1,0,0")
+
+#o << 0x90.chr + 0x64.chr + 0x64.chr
+#sleep 3
+#o << 0x80.chr + 0x64.chr + 0x00.chr
+
+#midi_in  :circuit, "circuit"
+#midi_out :circuit, "circuit"
+#midi_io  :korg,    "ms20mini"
+
+#plug :circuit => :korg do |n, korg| 
+#    n.pp
+#    korg << n
+#end
+
+#plug :korg do |n| 
+#    n.pp
+#end
