18364c1db9cbf921645fb7b0f70e708e902386c6
Alex Zhukov <baron.pampa@gmail.com>
2018-01-09 22:20:08 +0300

    decouple sequencer from drums

--- rack/x0x.rb
+++ rack/x0x.rb
@@ -1,62 +1,28 @@
-class X0X
-  def initialize(out)
-    @out   = out
-    @run   = false
-    @pos   = 0
-    @seq   = {}
-    @song  = []
-    yield(self)
+require_relative "./sequencer"
+
+class X0X < Sequencer
+  def initialize(*a)
+    @drum = {
+      :drum1 => 0x3c,
+      :drum2 => 0x3e,
+      :drum3 => 0x40,
+      :drum4 => 0x41
+    }
+    super(*a)
   end
 
-  def <<(m)
-    if m.start?
-      @run   = true
-      @pos   = 0
+  def method_missing(meth,*a, **kw)
+    if meth.to_s.start_with?("drum")
+      drum(@drum[meth], *a, **kw)
+    else
+      #FIXME raise MethodMissing
+      raise "Shit Happens"
     end
-    @run = false if m.stop?
-    pulse if @run && m.pulse?
-  end
 
-  def pulse 
-    if @pos >= @seq[@song[0]].length
-      @pos = 0
-      @song << @song.shift
-    end
-    _r = @seq[@song[0]][@pos]
-    @pos += 1
-    @out << _r unless _r.nil?
-  end
-
-  def part(part)
-    @_part = part
-    @song << part 
-    yield(self)
-    @_part = nil 
-  end
-
-  def song(*a)
-    @song = a.flatten
-  end
-
-  def drum1(s, **kw)
-    drum(0x3c, s, **kw)
-  end
-  
-  def drum2(s, **kw)
-    drum(0x3e, s, **kw)
-  end
-  
-  def drum3(s, **kw)
-    drum(0x40, s, **kw)
-  end
-  
-  def drum4(s, **kw)
-    drum(0x41, s, **kw)
   end
 
   def drum(byte, s, step: 6)
     _s = s.each_byte.reject { |c| [" ", "|"].include?(c.chr) }
-    raise "Sequence length #{_s.length * step} % 96 != 0, #{s}" unless (_s.length * step) % 96 == 0
     merge_seq (_s.map do |i|
       if i.chr == "-"
         [nil] * step 
@@ -67,14 +33,15 @@ class X0X
   end
 
   def merge_seq(s)
-    @seq[@_part] = [nil] * s.length if @seq[@_part].nil?
-    raise "seq.length #{@seq[@_part].length} != #{s.length}" unless @seq[@_part].length == s.length
-    for i in 0..(@seq[@_part].length - 1)
-      if @seq[@_part][i].nil?
-        @seq[@_part][i] = s[i]
+    @_seq = [nil] * s.length if @_seq.nil?
+    raise "seq.length #{@_seq.length} != #{s.length}" unless @_seq.length == s.length
+    for i in 0..(@_seq.length - 1)
+      if @_seq[i].nil?
+        @_seq[i] = s[i]
       elsif !s[i].nil?
-        @seq[@_part][i] += s[i]
+        @_seq[i] += s[i]
       end
     end
+    @_seq
   end
 end
--- track0.rb
+++ track0.rb
@@ -2,8 +2,6 @@ require "./rack/plug"
 require "./rack/x0x"
 
 circ = Plug.new("Circuit", "Circuit MIDI 1")
-# korg = Plug.new("MS-20 mini", "MS-20 mini MIDI 1")
-# euro = Synth.new("UM-ONE",     "UM-ONE MIDI 1")
 
 x0x = X0X.new(circ.out) do |x|
   x.part :a do |x|
@@ -11,12 +9,14 @@ x0x = X0X.new(circ.out) do |x|
     x.drum2 "| ---- S--- ---- S--- | ---- S--- ---- S--- |"
     x.drum4 "| H-H- H-H- H-H- H-H- | H-H- H-H- H-H- H-H- |"
   end
-  
   x.part :b do |x|
     x.drum1 "| KKKK | KKKK |", step: 24
     x.drum2 "| ---- S--- ---- S--- | ---- S--- ---- S--- |"
     x.drum4 "| H-H-H- H-H-H- H-H-H- H-H-H- | H-H-H- H-H-H- H-H-H- H-H-H- |", step: 4
   end
+  a = [:a,:b]
+  b = [:b,:a]
+  x.song a, b
 end
 
 circ.input :type => :clock do |m|
