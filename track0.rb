a552683e2d2e7c8d3423e1106d033f247ad49f6c
Alex Zhukov <baron.pampa@gmail.com>
2018-01-10 18:32:30 +0300

    try some ethnic rhythms

--- track0.rb
+++ track0.rb
@@ -4,19 +4,26 @@ require "./rack/x0x"
 circ = Plug.new("Circuit", "Circuit MIDI 1")
 
 x0x = X0X.new(circ.out) do |x|
-  x.part :a do |p|
-    p.drum1 "| KKKK | KKKK |", step: 24
-    p.drum2 "| ---- S--- ---- S--- | ---- S--- ---- S--- |"
-    p.drum4 "| H-H- H-H- H-H- H-H- | H-H- H-H- H-H- H-H- |"
+  x.part :four do |p|
+    p.drum1 "| K--- K--- K--- K--- |"
+    p.drum2 "| ---- S--- ---- S--- |"
+    p.drum4 "| H-H- H-H- H-H- H-H- |"
   end
-  x.part :b do |p|
-    p.drum1 "| KKKK | KKKK |", step: 24
-    p.drum2 "| ---- S--- ---- S--- | ---- S--- ---- S--- |"
-    p.drum4 "| H-H-H- H-H-H- H-H-H- H-H-H- | H-H-H- H-H-H- H-H-H- H-H-H- |", step: 4
+  
+  x.part :clave do |p|
+    p.drum1 "| K--K  --K-  --K-  K--- |"
   end
-  a = [:a,:b]
-  b = [:b,:a]
-  x.song a, b
+
+  x.part :bembe do |p|
+    p.drum1 "| K-K-  KK-K  -K-K |"
+  end
+
+  x.part :shiko do |p|
+    p.drum1 "| K---  K-K-  --K-  K---"
+  end
+  x.song [:four]  * 4,
+         [:clave] * 4,
+         [:shiko] * 4
 end
 
 circ.input :type => :clock do |m|
