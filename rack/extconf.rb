61eefed28981fec26cd3bdcb2a9037aa08e4c241
Alex Zhukov <baron.pampa@gmail.com>
2018-12-05 00:19:35 +0300

    remove old alsa sequencer

--- rack/extconf.rb
+++
@@ -1,6 +0,0 @@
-require 'mkmf'
-
-$CFLAGS     += ' -Wall -Werror -g -std=gnu11 -Wno-error=unused-result -Wno-error=unused-but-set-variable'
-$LOCAL_LIBS += `pkg-config --libs alsa`
-
-create_makefile("alsa_ext")
